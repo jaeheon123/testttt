e71aac1ad501eaf3864a16727bebd73eb0e2d1e5
안하령 <tmxlcl0020@naver.com>
2024-08-04 21:16:51 +0900

    초기화

---
+++ src/main/java/com/itbank/www/ItbankApplication.java
@@ -0,0 +1,13 @@
+package com.itbank.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itbank/www/ItbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itbank.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
